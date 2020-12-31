9307456f4b0d5719c9abedf22e014ac2e6147a5f
keyvan-alishiri <66223520+keyvan-alishiri@users.noreply.github.com>
2020-12-31 07:06:24 +0330

    Add project files.

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,17 @@
+﻿using FriendOrganizer.Model;
+using System.Collections.Generic;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            yield return new Friend { FirstName = "keyvan", LastName = "Alishir" };
+            yield return new Friend { FirstName = "peyman", LastName = "Alishir" };
+            yield return new Friend { FirstName = "hamed", LastName = "Alishir" };
+        }
+    }
+
+
+}
