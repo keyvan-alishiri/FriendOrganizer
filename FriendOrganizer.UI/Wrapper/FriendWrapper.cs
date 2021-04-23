436d4fc97b41c58ee6f2fe0b5fc4547df13f78ad
keyvan-alishiri <66223520+keyvan-alishiri@users.noreply.github.com>
2021-04-24 04:07:58 +0430

    Create a NotufyDataErrorInfo Base class

--- FriendOrganizer.UI/Wrapper/FriendWrapper.cs
+++ FriendOrganizer.UI/Wrapper/FriendWrapper.cs
@@ -1,16 +1,11 @@
 ﻿using FriendOrganizer.Model;
-using FriendOrganizer.UI.ViewModel;
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FriendOrganizer.UI.Wrapper
 {
-   public class FriendWrapper : ViewModelBase, INotifyDataErrorInfo
+   public class FriendWrapper : NotifyDataErrorInfoBase
    {
 	  public Friend Model { get; }
 	  public FriendWrapper(Friend model)
@@ -68,44 +63,7 @@ namespace FriendOrganizer.UI.Wrapper
 		 }
 	  }
 
-	  private Dictionary<string, List<string>> _errorsByPropertyName
-		  = new Dictionary<string, List<string>>();
-	  public bool HasErrors => _errorsByPropertyName.Any();
-
-	  public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
-
-	  public IEnumerable GetErrors(string propertyName)
-	  {
-		 return _errorsByPropertyName.ContainsKey(propertyName)
-			 ? _errorsByPropertyName[propertyName]
-			 : null;
-	  }
-
-	  private void OnErrorChanged(string propertyName)
-	  {
-		 ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
-	  }
-
-	  private void AddError(string propertyName, string error)
-	  {
-		 if (!_errorsByPropertyName.ContainsKey(propertyName))
-		 {
-			_errorsByPropertyName[propertyName] = new List<string>();
-		 }
-		 if (!_errorsByPropertyName[propertyName].Contains(error))
-		 {
-			_errorsByPropertyName[propertyName].Add(error);
-			OnErrorChanged(propertyName);
-		 }
-	  }
-	  private void ClearErrors(string propertyName)
-	  {
-		 if (!_errorsByPropertyName.ContainsKey(propertyName))
-		 {
-			_errorsByPropertyName.Remove(propertyName);
-			OnErrorChanged(propertyName);
-		 }
-	  }
+	 
 
    }
 }
