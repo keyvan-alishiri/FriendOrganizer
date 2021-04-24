6ce697fd88b1805337a9057dca7ca0e301e42e4a
keyvan-alishiri <66223520+keyvan-alishiri@users.noreply.github.com>
2021-04-24 04:36:16 +0430

    Introduce a ModelWrapper Class Trigger Validation in the ModelWrapper

--- FriendOrganizer.UI/Wrapper/FriendWrapper.cs
+++ FriendOrganizer.UI/Wrapper/FriendWrapper.cs
@@ -1,69 +1,52 @@
 ﻿using FriendOrganizer.Model;
 using System;
+using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FriendOrganizer.UI.Wrapper
 {
-   public class FriendWrapper : NotifyDataErrorInfoBase
+   public class FriendWrapper : ModelWrapper<Friend>
    {
-	  public Friend Model { get; }
-	  public FriendWrapper(Friend model)
+	  
+	  public FriendWrapper(Friend model) :base(model)
 	  {
-		 Model = model;
+		 
 	  }
 
 
 	  public int Id { get { return Model.Id; } }
 	  public string FirstName
 	  {
-		 get { return Model.FirstName; }
-		 set
-		 {
-			Model.FirstName = value;
-			OnPropertyChanged();
-			Validateproperty(nameof(FirstName));
-
-		 }
+		 get { return GetValue<string>()  ; }
+		 set{SetValue(value); }
 	  }
 
-	  private void Validateproperty(string propertyName)
-	  {
-		 ClearErrors(propertyName);
-		 switch (propertyName)
-		 {
-			case nameof(FirstName):
-			   if (string.Equals(FirstName, "Robot", StringComparison.OrdinalIgnoreCase))
-			   {
-				  AddError(propertyName, "Robots are not valid friends");
-			   }
-			   break;
-		 }
-	  }
-
+	
 	  public string LastName
 	  {
-		 get { return Model.LastName; }
-		 set
-		 {
-			Model.LastName = value;
-			OnPropertyChanged();
-
-		 }
+		 get { return GetValue<string>(); }
+		 set{ SetValue(value); }
 	  }
 
 	  public string Email
 	  {
-		 get { return Model.Email; }
-		 set
-		 {
-			Model.Email = value;
-			OnPropertyChanged();
-
-		 }
+		 get { return GetValue<string>(); }
+		 set {SetValue(value); }
 	  }
 
-	 
+	  protected override IEnumerable<string> ValidateProperty(string propertyName)
+	  {
+		 switch (propertyName)
+		 {
+		 case nameof(FirstName):
+		    if (string.Equals(FirstName, "Robot", StringComparison.OrdinalIgnoreCase))
+			{
+		 	  yield return "Robots are not valid friends";
+			}
+			break;
+		 }
+	  }
 
    }
 }
