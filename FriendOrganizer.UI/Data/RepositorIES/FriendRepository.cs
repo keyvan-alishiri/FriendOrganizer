545a81c507d3637138513e415242eeb21b22c154
keyvan-alishiri <66223520+keyvan-alishiri@users.noreply.github.com>
2021-04-29 21:15:04 +0430

    Create a GenericRepository Class

--- FriendOrganizer.UI/Data/RepositorIES/FriendRepository.cs
+++ FriendOrganizer.UI/Data/RepositorIES/FriendRepository.cs
@@ -1,6 +1,5 @@
 ﻿using FriendOrganizer.DataAccess;
 using FriendOrganizer.Model;
-using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -8,53 +7,39 @@ using System.Threading.Tasks;
 
 namespace FriendOrganizer.UI.Data.Repositories
 {
-   public class FriendRepository : IFriendRepository
+
+
+
+   public class FriendRepository : GenericRepository<Friend,FriendOrganizerDbContext> ,
+	                               IFriendRepository
    {
 
-	  private FriendOrganizerDbContext _context;
+	
 
-	  public FriendRepository(FriendOrganizerDbContext context)
+	  public FriendRepository(FriendOrganizerDbContext context) :base(context)
 	  {
-		 _context = context;
+		
 	  }
 
-	  public void Add(Friend friend)
-	  {
-		 _context.Friends.Add(friend);
-	  }
 
-	  public async Task<Friend> GetByIdAsync(int friendId)
+
+	  public override async Task<Friend> GetByIdAsync(int friendId)
 	  {
 
-		 return await _context.Friends
+		 return await Context.Friends
 			.Include(f=>f.PhoneNumbers)
 			.SingleOrDefaultAsync(f => f.Id == friendId);
 
 	  }
 
-	  public bool HasChanges()
-	  {
-		 return _context.ChangeTracker.HasChanges();
-	  }
 
-	  public void Remove(Friend model)
-	  {
-		 _context.Friends.Remove(model);
-		 
-	  }
 
 	  public void RemovePhoneNumber(FriendPhoneNumber model)
 	  {
-		 _context.FriendPhoneNumbers.Remove(model);
+		 Context.FriendPhoneNumbers.Remove(model);
 	  }
 
-	  public async Task SaveAsync()
-	  {
-
-
-		 await _context.SaveChangesAsync();
-
-	  }
+	
    }
 
 
