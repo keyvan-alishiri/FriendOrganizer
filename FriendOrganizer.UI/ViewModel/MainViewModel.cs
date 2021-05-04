d06939286478e47faa50d4b2263461bc3a8341af
keyvan-alishiri <66223520+keyvan-alishiri@users.noreply.github.com>
2021-05-05 03:37:46 +0430

    Subscribe to the Event to Close the Tab

--- FriendOrganizer.UI/ViewModel/MainViewModel.cs
+++ FriendOrganizer.UI/ViewModel/MainViewModel.cs
@@ -50,21 +50,33 @@ namespace FriendOrganizer.UI.ViewModel
 			   .Subscribe(OnOpenDetailView);
 		 _eventAggregator.GetEvent<AfterDetailDeletedEvent>()
 			.Subscribe(AfterDetailDeleted);
+		 _eventAggregator.GetEvent<AfterDetailClosedEvent>()
+			.Subscribe(AfterDetailClosed);
 		 CreateNewDetailCommand = new DelegateCommand<Type>(OnCreateNewDetailExecute);
 
 
 	  }
 
+	  private void AfterDetailClosed(AfterDetailClosedEventArgs args)
+	  {
+		 RemoveDetailViewModel(args.Id, args.ViewModelName);
+	  }
+
 	  private void AfterDetailDeleted(AfterDetailDeletedEventArgs args)
 	  {
-		 var detailViewModel = DetailViewModels.SingleOrDefault(vm => vm.Id == args.Id && vm.GetType().Name == args.ViewModelName);
+		 RemoveDetailViewModel(args.Id,args.ViewModelName);
+
+	  }
+
+	  private void RemoveDetailViewModel(int id, string viewModelName)
+	  {
+		 var detailViewModel = DetailViewModels.SingleOrDefault(vm => vm.Id ==id && vm.GetType().Name == viewModelName);
 		 if (detailViewModel != null)
 		 {
 			DetailViewModels.Remove(detailViewModel);
 		 }
-
-
 	  }
+
 
 	  private async void OnOpenDetailView(OpenDetailViewEventArgs args)
 	  {
